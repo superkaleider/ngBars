76b3399e3708b8681f72a231207f9d8b5bc17805
Luke Bond <Superkaleider@unknown.home>
2017-02-15 20:43:18 +0000

    some changes

--- app/scripts/directives/barChart.js
+++ app/scripts/directives/barChart.js
@@ -11,13 +11,24 @@
           height: "=",
           width: "="
         },
-        templateUrl:'partials/barchart.html',
+        templateUrl: 'partials/barchart.html',
         link: function (scope, element, attrs) {
-
-          $timeout(function () {}, 200);
+          scope.color = function (id) {
+            var colors = [
+              "#c62828",
+              "#3f51b5",
+              "#457e45",
+              "#825b11",
+              "#8044cc",
+              "#ff9105"
+            ];
+            console.log(colors[id]);
+            return colors[id];
+          }
+          // $timeout(function () {}, 200);
 
           function generate() {
-            // element.empty();
+            // element.empty(); 
             var cd = alasql('SELECT label, SUM(v) AS val FROM ? GROUP BY label', [scope.data]);
             scope.bars = cd;
             var count = 0;
@@ -37,49 +48,38 @@
             } else {
               var barheight = scope.height / count || element.css('height');
             }
-            var height = scope.height || element.css('height');
-            var width = scope.width || element.css('width');
-            var chartContainer = angular.element('<div class="chartContainer"></div>');
-            chartContainer.css('width', width + 'px');
-            chartContainer.css('height', height + 'px');
-            if (scope.chartName) {
-              var chartLabel = angular.element('<h5 class="chartName">' + scope.chartName + '</h5>');
-              chartContainer.append(chartLabel);
-            }
+            // var height = scope.height || element.css('height');
+            // var width = scope.width || element.css('width');
+            // var chartContainer = angular.element('<div class="chartContainer"></div>');
+            // chartContainer.css('width', width + 'px');
+            // chartContainer.css('height', height + 'px');
+            // if (scope.chartName) {
+            //   var chartLabel = angular.element('<h5 class="chartName">' + scope.chartName + '</h5>');
+            //   // chartContainer.append(chartLabel);
+            // }
             var index = 0;
-            angular.forEach(cd, function (d) {
-              index++;
-              var container = angular.element('<div class="series"></div>');
-              var label = angular.element('<div class="chartSeries">' + d.label + '</div>');
-              label.css('width', width + 'px');
-              var bar = angular.element('<div class="bar"></div>');
-              bar.css('z-index', count - index);
-              container.css("width", (d.val / max * width) + 'px');
-              container.css("height", barheight + 'px');
-              container.css("background-color", color());
+            // angular.forEach(cd, function (d) {
+            // index++;
+            // var container = angular.element('<div class="series"></div>');
+            // var label = angular.element('<div class="chartSeries">' + d.label + '</div>');
+            // label.css('width', width + 'px');
+            // var bar = angular.element('<div class="bar"></div>');
+            // bar.css('z-index', count - index);
+            // container.css("width", (d.val / max * width) + 'px');
+            // container.css("height", barheight + 'px');
+            // container.css("background-color", color());
 
-              var valueLabel = angular.element('<label class="chartLabels">' + d.val + '</label>');
+            // var valueLabel = angular.element('<label class="chartLabels">' + d.val + '</label>');
 
-              container.append(label);
-              container.append(bar);
-              bar.append(valueLabel);
-              chartContainer.append(container);
-            })
-            element.append(chartContainer);
+            // container.append(label);
+            // container.append(bar);
+            // bar.append(valueLabel);
+            // chartContainer.append(container);
+            // })
+            // element.append(chartContainer);
           }
 
-          function color() {
-            var r = Math.floor(Math.random() * 6);
-            var colors = [
-              "#c62828",
-              "#3f51b5",
-              "#457e45",
-              "#825b11",
-              "#8044cc",
-              "#ff9105"
-            ];
-            return colors[r];
-          }
+
 
           scope.$watch('data', function (n, o) {
             // console.log('new', n, 'old', o);
